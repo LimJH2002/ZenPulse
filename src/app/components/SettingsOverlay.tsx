09cdd7369e263033ae6011aac6508d767d02ccb5
L1MJH <37625953+LimJH2002@users.noreply.github.com>
2023-12-04 21:45:25 +0800

    Make background responsive

--- src/app/components/SettingsOverlay.tsx
+++ src/app/components/SettingsOverlay.tsx
@@ -3,10 +3,6 @@ import { XMarkIcon } from "@heroicons/react/24/outline";
 import { Fragment } from "react";
 import Sidebar from "./Sidebar";
 
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
-
 interface SettingsOverlayProps {
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -19,6 +15,17 @@ export default function SettingsOverlay({
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-50 transition-opacity" />
+        </Transition.Child>
         <div className="fixed inset-0 overflow-hidden">
           <div className="absolute inset-0 overflow-hidden">
             <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
@@ -31,7 +38,7 @@ export default function SettingsOverlay({
                 leaveFrom="translate-x-0"
                 leaveTo="translate-x-full"
               >
-                <Dialog.Panel className="pointer-events-auto w-screen max-w-2xl">
+                <Dialog.Panel className="pointer-events-auto w-screen max-w-3xl">
                   <div className="flex h-full flex-col overflow-y-scroll bg-gray-900 shadow-xl">
                     <div className="px-4 py-6 sm:px-6">
                       <div className="flex items-start justify-between">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import BottomNav from "./components/BottomNav";
 
 export default function Home() {
   return (
-    <main className="bg-main-bg bg-cover bg-center h-screen bg-black">
+    <main className="bg-main-bg bg-center bg-cover h-screen w-screen bg-black overflow-hidden">
       {/* Overlay */}
       <div className="absolute inset-0 bg-black bg-opacity-30" />
 
       {/* Content */}
       <div className="flex flex-col items-center justify-center h-full relative z-10">
         <div className="text-white text-center">
-          <p className="text-3xl">
+          <p className="text-xl mb-10 sm:text-3xl sm:mb-5 mx-2">
             Embrace the weekend, Jian Hong. You aced Friday.
           </p>
           <Clock />
