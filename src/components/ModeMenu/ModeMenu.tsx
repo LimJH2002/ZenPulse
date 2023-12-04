a1b642a22bd20d9b86444e0f251817a1757f8c44
L1MJH <37625953+LimJH2002@users.noreply.github.com>
2023-12-04 23:44:38 +0800

    Fix UI for ModeMenu

--- src/components/ModeMenu/ModeMenu.tsx
+++ src/components/ModeMenu/ModeMenu.tsx
@@ -1,5 +1,5 @@
 // components/ModeMenu.tsx
-import { faMessage, faMusic } from "@fortawesome/free-solid-svg-icons";
+import { faCode, faLightbulb, faHome, faC } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import styles from "./ModeMenu.module.css";
@@ -10,27 +10,27 @@ function ModeMenu() {
   const sliderOffset = (selectedIcon - 1) * iconWidthRem;
 
   return (
-    <div className="flex items-center rounded-3xl border-2 border-white border-opacity-100 mr-4">
+    <div className="flex items-center rounded-full border-2 border-white border-opacity-100 mr-4">
       <div
         className={styles.slider}
         style={{ transform: `translateX(${sliderOffset}rem)` }}
       />
       {/* Music Icon */}
       <FontAwesomeIcon
-        icon={faMusic}
-        className="text-white text-2xl cursor-pointer hover:text-gray-300 ml-4 my-3 z-10"
+        icon={faHome}
+        className="text-white text-2xl cursor-pointer hover:text-gray-300 ml-3 my-3 z-10 w-6"
         onClick={() => setSelectedIcon(1)}
       />
       {/* Message Icon */}
       <FontAwesomeIcon
-        icon={faMessage}
-        className="text-white text-2xl cursor-pointer hover:text-gray-300 mx-6 my-3 z-10"
+        icon={faLightbulb}
+        className="text-white text-2xl cursor-pointer hover:text-gray-300 mx-6 my-3 z-10 w-6"
         onClick={() => setSelectedIcon(2)}
       />
       {/* Another Message Icon */}
       <FontAwesomeIcon
-        icon={faMessage}
-        className="text-white text-2xl cursor-pointer hover:text-gray-300 mr-4 my-3 z-10"
+        icon={faCode}
+        className="text-white text-2xl cursor-pointer hover:text-gray-300 mr-3 my-3 z-10 w-6"
         onClick={() => setSelectedIcon(3)}
       />
     </div>
